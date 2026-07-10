3ce27fade6de290c53d89acdc964bb2f6617e86e
rva3 <rva333@protonmail.com>
2026-07-10 21:36:28 +0300

    board: mediatek: mt6572: jty-d101: register USB gadget
    Signed-off-by: rva3 <rva333@protonmail.com>

--- board/mediatek/mt6572/jty-d101.c
+++ board/mediatek/mt6572/jty-d101.c
@@ -1,12 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
-#include <clk.h>
-#include <config.h>
+#include <asm/global_data.h>
 #include <dm.h>
 #include <init.h>
-#include <log.h>
-#include <asm/global_data.h>
-#include <asm/io.h>
-#include <linux/printk.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -15,8 +10,22 @@ int board_init(void)
 	return 0;
 }
 
+int board_late_init(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = uclass_get_device(UCLASS_USB_GADGET_GENERIC, 0, &dev);
+	if (ret) {
+		printf("%s: failed to get USB: %d\n", __func__, ret);
+		return ret;
+	}
+
+	return 0;
+}
+
 /* LK framebuffer */
 phys_addr_t board_get_usable_ram_top(phys_addr_t total_size)
 {
-    return 0xbf400000;
+	return 0xbf400000;
 }
